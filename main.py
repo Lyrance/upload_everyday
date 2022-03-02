91aaa147ad8614bc464bbf1d1524d3a4580df516
lyrance <lyrance@163.com>
2022-03-02 20:33:20 +0800

    加入json

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # !/usr/bin/python
 import importlib, sys
 import time
-
 importlib.reload(sys)
 import requests, json, base64, hashlib
 import smtplib
@@ -12,16 +11,22 @@ import datetime
 
 
 class antlinker(object):
-    def __init__(self):
-        self.usr = ''  # 手机号
-        self.pwd = ''  # 密码
+    def __init__(self, person):
+        self.usr = person['phone']
+        self.pwd = person['password']
+        self.college = person['college']
+        self.grade = person['grade']
+        self.major = person['major']
+        self.clas = person['clas']
+        self.id = person['id']
+        self.name = person['name']
+
         self.s = requests.Session()
         self.headers = {
             'User-Agent': 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 11; MI 10 MIUI/21.1.13)',
             'Authorization': 'BASIC '
                              'NTgyYWFhZTU5N2Q1YjE2ZTU4NjhlZjVmOmRiMzU3YmRiNmYzYTBjNzJkYzJkOWM5MjkzMmFkMDYyZWRkZWE5ZjY='
         }
-
     # 登录 获取token
     def get_token(self):
         usr = "{\"LoginModel\":1,\"Service\":\"ANT\",\"UserName\":\"%s\"}" % self.usr
@@ -100,8 +105,28 @@ class antlinker(object):
                 continue
 
             #上报
-            data = "{\"Body\" : \"{\\\"Field\\\":[{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"学院\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"年级\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"专业\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"班级\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"学号\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"姓名\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}],\\\"TaskCode\\\":\\\"" + \
-                   TaskCode + "\\\",\\\"TemplateId\\\":\\\"0e284f0a-5025-4d41-b9cc-d69b3deea5d3\\\"}\",  \"Router\" : \"\/api\/newcustomerform\/submit\",  \"Method\" : \"POST\"}"
+            data = "{\"Body\" : \"{\\\"Field\\\":[{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.college + \
+                   "\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.grade + \
+                   "\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.major + \
+                   "\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.clas +  \
+                   "\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.id +    \
+                   "\\\"},{\\\"FieldCode\\\":\\\"disabled\\\",\\\"Content\\\":\\\"" + self.name +  \
+                   "\\\"},{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"< 37.3℃\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}," \
+                   "{\\\"FieldCode\\\":\\\"\\\",\\\"Content\\\":\\\"否\\\"}],\\\"TaskCode\\\":\\\"" + \
+                   TaskCode + "\\\",\\\"TemplateId\\\":\\\"0e284f0a-5025-4d41-b9cc-d69b3deea5d3\\\"}\",  " \
+                              "\"Router\" : \"\/api\/newcustomerform\/submit\",  \"Method\" : \"POST\"}"
             upload = self.s.post(url, headers=self.headers, data=data.encode('utf-8'))
             response = json.loads(upload.text)
             try:
@@ -128,39 +153,44 @@ class antlinker(object):
             feedback = str(reason)
         return feedback
 
+with open("info.json", 'r', encoding="UTF-8") as load_f:
+    load_dict = json.load(load_f)
 
-gogogo = antlinker()
-try:
-    gogogo.refresh_token()
-except:
-    gogogo.get_token()
-state = gogogo.upload_info()
-#gogogo.upload_temperature()
+for person in load_dict:
+    p1 = load_dict[person]
+    gogogo = antlinker(p1)
+    try:
+        gogogo.refresh_token()
+    except:
+        gogogo.get_token()
+    state = gogogo.upload_info()
+    gogogo.upload_temperature()
+    time.sleep(10)
 
 # 获取时间信息
-time1 = datetime.datetime.now()
-time1_str = datetime.datetime.strftime(time1, '%Y-%m-%d %H:%M:%S')
-
-if state == True:
-    mess = time1_str + "成功"
-else:
-    mess = time1_str + "失败"
-    mess = "" + mess
-    mail_host = "smtp.163.com"  # 设置服务器
-    mail_user = ""  # 用户名
-    mail_pass = ""  # 口令
-
-    sender = ''
-    receivers = ['']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-
-    message = MIMEText(mess, 'plain', 'utf-8')
-    message['From'] = Header(" ", 'utf-8')
-    message['To'] = Header(" ", 'utf-8')
-
-    subject = mess
-    message['Subject'] = Header(subject, 'utf-8')
-
-    smtpObj = smtplib.SMTP()
-    smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
-    smtpObj.login(mail_user, mail_pass)
-    smtpObj.sendmail(sender, receivers, message.as_string())
+# time1 = datetime.datetime.now()
+# time1_str = datetime.datetime.strftime(time1, '%Y-%m-%d %H:%M:%S')
+#
+# if state == True:
+#     mess = time1_str + "成功"
+# else:
+#     mess = time1_str + "失败"
+#     mess = "" + mess
+#     mail_host = "smtp.163.com"  # 设置服务器
+#     mail_user = ""  # 用户名
+#     mail_pass = ""  # 口令
+#
+#     sender = ''
+#     receivers = ['']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+#
+#     message = MIMEText(mess, 'plain', 'utf-8')
+#     message['From'] = Header(" ", 'utf-8')
+#     message['To'] = Header(" ", 'utf-8')
+#
+#     subject = mess
+#     message['Subject'] = Header(subject, 'utf-8')
+#
+#     smtpObj = smtplib.SMTP()
+#     smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+#     smtpObj.login(mail_user, mail_pass)
+#     smtpObj.sendmail(sender, receivers, message.as_string())
